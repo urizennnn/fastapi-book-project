d6f9b7cecd915a4cca35b74fff061fda3373ca95
urizennnn <igamerryt@gmail.com>
2025-02-10 16:54:28 +0100

    chore:added endpoint to get books

--- api/routes/books.py
+++ api/routes/books.py
@@ -36,16 +36,18 @@ db.books = {
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_book(book: Book):
     db.add_book(book)
-    return JSONResponse(
-        status_code=status.HTTP_201_CREATED, content=book.model_dump()
-    )
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=book.model_dump())
 
 
-@router.get(
-    "/", response_model=OrderedDict[int, Book], status_code=status.HTTP_200_OK
-)
+@router.get("/", response_model=OrderedDict[int, Book], status_code=status.HTTP_200_OK)
 async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
+
+
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int):
+    book = db.get_book(book_id)
+    return JSONResponse(status_code=status.HTTP_200_OK, content=book.model_dump())
 
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
